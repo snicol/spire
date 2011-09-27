aa5a7179451847ee956eb304afa30bc59ad64f0c
Scott Nicol <scott@scottnicol.co.uk>
2011-09-27 19:36:30 +0100

    removed the base_path from being outputted on every request

--- lib/spire/class/MainController.rb
+++ lib/spire/class/MainController.rb
@@ -10,10 +10,8 @@ module Spire
     
     def render(view, type="haml", data="")
       if type == "html"
-        puts @base_path
         File.open(@base_path+'/views/'+view+'.html', 'r').read
       elsif type == "haml"
-        puts @base_path
         file = File.open(@base_path+'/views/'+view+'.haml', 'r').read
         Haml::Engine.new(file).render(Object.new, :data => data)
       end

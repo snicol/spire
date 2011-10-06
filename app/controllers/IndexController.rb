f8fccb78da7b9741ce512b588c8ebbc3c830e83c
Scott Nicol <scott@scottnicol.co.uk>
2011-10-06 10:17:06 +0000

    removed files for gitignore

--- app/controllers/IndexController.rb
+++
@@ -1,6 +0,0 @@
-class Index < Spire::MainController
-  def index
-    Spire::Response.new(render("index", "haml", ["Welcome to Spire!", 
-      "This is a base app to extend your own on. Have fun!"]))
-  end
-end
